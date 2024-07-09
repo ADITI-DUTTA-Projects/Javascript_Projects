638501c6ff648b5d18361c5489c7c759216421d4
ADITI DUTTA <143431803+ADITI-DUTTA-Projects@users.noreply.github.com>
2024-07-09 21:17:15 +0530

    Uploaded Keycode project file

---
+++ 05_keycode/index.js
@@ -0,0 +1,20 @@
+// console.log('Project 5');
+const insert = document.querySelector('#insert');
+window.addEventListener('keydown', (e) => {
+    insert.innerHTML = `
+  <div class = "color">
+  <table>
+  <tr>
+    <th>key</th>
+    <th>keycode</th>
+    <th>code</th>
+  </tr>
+  <tr>
+    <td>${e.key === ' ' ? 'space' : e.key}</td>
+    <td>${e.keycode}</td>
+    <td>${e.code}</td>
+  </tr>
+</table>
+  </div>
+  `;
+});
